7ba8897f4ea4e647e125ba2648ff016cc0fc1639
Nirjaraa <nirjarathapa11@gmail.com>
2023-12-09 22:31:40 +0545

    FIXED BUG SIGNIN

--- signin.cpp
+++ signin.cpp
@@ -17,41 +17,8 @@ signin::~signin()
 
 
         //connection with database-----------------------------------------------------------------------------------------------
-//    hide();
-//    QString userId, passwrd,first,last,email;
-//    userId = ui->uname->text();
-//    passwrd= ui->pw->text();
-//    first= ui->firstname->text();
-//    last= ui->lastname->text();
-//    email= ui->emaill->text();
-
-//    if(!connOpen())
-//    {
-//        ui->label_4->setText("Not connected to database");
-//            //add a fucntion to end the application
-//    }
-//    connOpen();
-//    QSqlQuery qry;
-//    qry.prepare("select * from logininfoo where username='"+userId+"' and pw='"+passwrd+"' and fname='"+first+"' and lname='"+last+"' and email='"+email+"'" );
-//    if(qry.exec()) // executing querry in
-//    {
-//    int count=0;          //this will the count the number of time it executes the querry
-//        while(qry.next())
-//    {
-//        count++;
-//    }
-//    if(count==1)
-//    {
-//        ui->label_4->setText("username and pw correct");
-
-//    connClose();
 
 
-   /* if(count<1)
-    {
-     ui->label_4->setText("incorrect");
-    }
-*/
 
 
 
@@ -62,9 +29,49 @@ signin::~signin()
 
 void signin::on_Signup_clicked()
 {
-        hide();
-        l=new linkin(this);
-        l->show();
 
+        QString userId, passwrd,first,last,email;
+        userId = ui->uname->text();
+        passwrd= ui->pw->text();
+        first= ui->firstname->text();
+        last= ui->lastname->text();
+        email= ui->emaill->text();
+
+        if(!connOpen())
+        {
+            ui->label_4->setText("Not connected to database");
+                //add a fucntion to end the application
+        }
+        connOpen();
+
+        QSqlQuery qry;
+        qry.prepare("select * from logininfoo where username='"+userId+"' and pw='"+passwrd+"' and fname='"+first+"' and lname='"+last+"' and email='"+email+"'" );
+
+        if(qry.exec())  // executing querry in
+        {
+        int count=0;          //this will the count the number of time it executes the querry
+            while(qry.next())
+        {
+            count++;
+        }
+        if(count==1)
+        {
+            ui->label_4->setText("username and pw correct");
+            hide();
+            l=new linkin(this);
+            l->show();
+        }
+        if(count<1)
+        {
+            ui->label_4->setText("incorrect");
+        }
+
+
+
+
+
+
+
+}
 }
 
